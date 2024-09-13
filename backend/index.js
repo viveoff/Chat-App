f5010fd09d7e09c16544420f38408031830dfe93
viveoff <vlad.shorodok14@gmail.com>
2024-09-13 20:23:04 +0200

    Added routes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try {
+    const r = await axios.put(
+      'https:/api.chatengine.io/users/',
+      {username: username, secret: username, first_name: username},
+      {headers: {"privat-key": "588c038c-f049-4297-8806-a0ed1996203b"}}
+    )
+    return res.status(r.status).json(r.data)
+  } catch(e) {
+    return res.status(e.response.status).json(e.response.data)
+  }
 });
 
-app.listen(3001);
+app.listen(3001); 
